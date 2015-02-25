8c361c8cc0c3913d0a1eda8664df8b5192fffdef
MacakM <410452@mail.muni.cz>
2015-02-25 20:40:45 +0100

    Added states and defined their handlers.

--- ZRTP/stateengine.cpp
+++ ZRTP/stateengine.cpp
@@ -6,7 +6,18 @@ StateEngine::StateEngine()
 
 void StateEngine::initHandlers()
 {
-
+    handlers[Initial] = &StateEngine::handleInitial;
+    handlers[SentHello] = &StateEngine::handleSentHello;
+    handlers[SentHelloAck] = &StateEngine::handleSentHelloAck;
+    handlers[ReceivedHelloAck] = &StateEngine::handleReceivedHelloAck;
+    handlers[SentCommit] = &StateEngine::handleSentCommit;
+    handlers[WaitCommit] = &StateEngine::handleWaitCommit;
+    handlers[WaitDHPart2] = &StateEngine::handleWaitDHPart2;
+    handlers[WaitConfirm1] = &StateEngine::handleWaitConfirm1;
+    handlers[WaitConfirm2] = &StateEngine::handleWaitConfirm2;
+    handlers[WaitConf2Ack] = &StateEngine::handleWaitConf2Ack;
+    handlers[Secured] = &StateEngine::handleSecured;
+    handlers[WaitErrorAck] = &StateEngine::handleWaitErrorAck;
 }
 
 void StateEngine::processEvent(Event *event)
--- ZRTP/stateengine.h
+++ ZRTP/stateengine.h
@@ -2,15 +2,24 @@
 #define STATEENGINE_H
 
 #include "Integers.h"
-#include "States.h"
 #include <map>
 
 class StateEngine;
 
 typedef enum
 {
-    Init            = 0x0000,
-    WaitHelloACK    = 0x0001
+    Initial             = 0x0000,
+    SentHello           = 0x0001,
+    SentHelloAck        = 0x0002,
+    ReceivedHelloAck    = 0x0003,
+    SentCommit          = 0x0004,
+    WaitCommit          = 0x0005,
+    WaitDHPart2         = 0x0006,
+    WaitConfirm1        = 0x0007,
+    WaitConfirm2        = 0x0008,
+    WaitConf2Ack        = 0x0009,
+    Secured             = 0x0010,
+    WaitErrorAck        = 0x0011
 }States;
 
 typedef enum
@@ -42,6 +51,19 @@ private:
 
     void initHandlers();
     void processEvent(Event *event);
+
+    void handleInitial();
+    void handleSentHello();
+    void handleSentHelloAck();
+    void handleReceivedHelloAck();
+    void handleSentCommit();
+    void handleWaitCommit();
+    void handleWaitDHPart2();
+    void handleWaitConfirm1();
+    void handleWaitConfirm2();
+    void handleWaitConf2Ack();
+    void handleSecured();
+    void handleWaitErrorAck();
 };
 
 #endif // STATEENGINE_H
